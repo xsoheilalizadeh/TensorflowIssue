d94623391df29a795ecd8efd970daf22c1860295
soheilalizadeh <xsoheilalizadeh@gmail.com>
2020-12-20 19:18:39 +0330

    Initial Commit

---
+++ TensorflowIssue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.ML;
+
+namespace TensorflowIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var context = new MLContext();
+
+          var model = context.Model.LoadTensorFlowModel("./model/saved_model.pb");
+        }
+    }
+}
